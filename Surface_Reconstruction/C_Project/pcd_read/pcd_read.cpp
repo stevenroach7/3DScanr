06a43de0d3eab1be23135a70378a63b5ccadc76a
hthaivu <hthaivu@macalester.edu>
2018-02-11 11:59:11 -0600

    add comments

--- Surface_Reconstruction/C_Project/pcd_read/pcd_read.cpp
+++ Surface_Reconstruction/C_Project/pcd_read/pcd_read.cpp
@@ -1,9 +1,16 @@
+/*
+This file is for reading a PCD pointcloud file "pcd_test.pcd"
+The out put are the xyz coordinates of every points in the file, to make sure the file was properly created
+
+Created by: Hoang Anh Thai Vu
+Updated on: February 11, 2018
+*/
+
 #include <iostream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 
-int
-main (int argc, char** argv)
+int main (int argc, char** argv)
 {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
 
