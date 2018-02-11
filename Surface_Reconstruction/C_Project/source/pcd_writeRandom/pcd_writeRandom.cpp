9cd96ad686d5cc5d9e731fb1e38971f36696dae5
hthaivu <hthaivu@macalester.edu>
2018-02-11 14:46:52 -0600

    string output

--- Surface_Reconstruction/C_Project/source/pcd_writeRandom/pcd_writeRandom.cpp
+++ Surface_Reconstruction/C_Project/source/pcd_writeRandom/pcd_writeRandom.cpp
@@ -2,28 +2,29 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 
-int  pcd_writeRandom ()
+using namespace std;
+
+string  pcd_writeRandom()
 {
-  pcl::PointCloud<pcl::PointXYZ> cloud;
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+	string cloudString;
+	// Fill in the cloud data
+	cloud.width = 5;
+	cloud.height = 1;
+	cloud.is_dense = false;
+	cloud.points.resize(cloud.width * cloud.height);
 
-  // Fill in the cloud data
-  cloud.width    = 5;
-  cloud.height   = 1;
-  cloud.is_dense = false;
-  cloud.points.resize (cloud.width * cloud.height);
+	for (size_t i = 0; i < cloud.points.size(); ++i)
+	{
+		cloud.points[i].x = 1024 * rand() / (RAND_MAX + 1.0f);
+		cloudString = to_string(cloud.points[i].x) + (string) " ";
 
-  for (size_t i = 0; i < cloud.points.size (); ++i)
-  {
-    cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
-  }
+		cloud.points[i].y = 1024 * rand() / (RAND_MAX + 1.0f);
+		cloudString = to_string(cloud.points[i].y) + (string) " ";
 
-  pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
-  std::cerr << "Saved " << cloud.points.size () << " data points to test_pcd.pcd." << std::endl;
+		cloud.points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
+		cloudString = to_string(cloud.points[i].z) + (string) " \n";
+	}
 
-  for (size_t i = 0; i < cloud.points.size (); ++i)
-    std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
-
-  return (0);
+	return cloudString;
 }
